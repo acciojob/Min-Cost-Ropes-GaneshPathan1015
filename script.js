4ed20a5f8b2b11625cd37713a953483d3ee3f4d3
Ganesh Pathan <118661019+GaneshPathan1015@users.noreply.github.com>
2023-12-12 14:39:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	arr.sort((a,b)=>{
+		return a-b;
+	});
+	let sum = arr[0],storsum=0;
+	for(let i=1;i<arr.length;i++){
+		sum = sum+arr[i];
+		storsum=storsum + sum;
+	}
+  return arr.length===1?sum:storsum;
 }
 
 module.exports=mincost;
